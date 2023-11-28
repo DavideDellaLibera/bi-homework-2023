32de1edebbfe9ffeae45fd2ac81e3e4b1dabc6b3
LetiziaLongoni <81821167+LetiziaLongoni@users.noreply.github.com>
2023-11-28 19:55:06 +0100

    Update domanda-5.sql
    Comments

--- queries/denorm/domanda-5.sql
+++ queries/denorm/domanda-5.sql
@@ -1,7 +1,7 @@
 
 /* DOMANDA 5 - DENORM */
 
-/* CALCOLO FAST AND FURIOUS INDEX */
+/* COMPUTE FAST AND FURIOUS INDEX */
 WITH fast_and_furious_index AS (
 	SELECT t3.*, 
 		ROUND(t3.mesi_att_norm * 0.25 + t3.tasso_successo * 0.25 + t3.voto_medio_norm * 0.25 + t3.n_esami_superati_norm * 0.25, 3) AS fast_and_furious
@@ -15,7 +15,7 @@ WITH fast_and_furious_index AS (
 				
 		FROM (
 
-			/* MESI (PERIODO) ATTIVITA STUDENTI (FAST) */
+			/* FAST PARAMETER: PERIOD OF ACTIVITY GIVEN BY THE DIFFERENCE BETWEEN FIRST AND LAST EXAM TAKEN BY EACH STUDENT */
 			SELECT cdscod, studente, 
 				CAST(ROUND((JULIANDAY(DATE(MAX(dtappello), 'start of month')) - JULIANDAY(DATE(MIN(dtappello), 'start of month'))) / 30) + 1 AS INTEGER) AS mesi_att
 			FROM bos_denormalizzato
@@ -25,12 +25,13 @@ WITH fast_and_furious_index AS (
 		) AS params_fast
 		INNER JOIN (
 
-			/* PARAMETRI FURIOUS */
+			/* FURIOUS PARMETERS: NUMBER OF PASSED EXAMS, AVERAGE GRADE AND SUCCESS RATIO  FOR EACH STUDENT*/
 			SELECT t1.cdscod, t1.studente, t1.n_esami_superati, 
 				(CASE WHEN t1.voto_medio IS NULL THEN 0 ELSE t1.voto_medio END) AS voto_medio,
 				(CAST(t1.n_esami_superati AS REAL) / CAST(n_iscrizioni AS REAL)) AS tasso_successo
 			FROM (
-				
+
+				/* NUMBER OF ENROLLMENTS, PASSED EXAMS AND AVERAGE GRADE FOR EACH STUDENT */
 				SELECT cdscod, studente, COUNT(*) AS n_iscrizioni, 
 					SUM(CASE WHEN Superamento = 1 THEN 1 ELSE 0 END) AS n_esami_superati, AVG(voto) AS voto_medio
 				FROM bos_denormalizzato
