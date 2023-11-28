b22f52057a24772274f36577218a059c285ab391
LetiziaLongoni <81821167+LetiziaLongoni@users.noreply.github.com>
2023-11-28 18:31:28 +0100

    Update domanda-1.sql
    Comments

--- queries/norm/domanda-1.sql
+++ queries/norm/domanda-1.sql
@@ -1,17 +1,19 @@
 
 /* QUERY 1 - NORM */
 
-/* VIEW: STATISTICHE APPELLI CON AGGREGAZIONE STESSI CDS, ADCOD E DATA */
+/* VIEW: NUMBER OF STUDENTS ENROLLED FOR EACH EXAM */
 DROP VIEW IF EXISTS stats_appelli_agg;
 CREATE VIEW IF NOT EXISTS stats_appelli_agg AS
 
+	/* NUMBER OF ENROLLED STUDENTS AT EACH EXAM */
 	SELECT stats_appelli.cdscod, stats_appelli.adcod, stats_appelli.dtappello, 
 		SUM(stats_appelli.numero_iscritti) AS numero_iscritti
 	FROM stats_appelli
-	GROUP BY stats_appelli.cdscod, stats_appelli.adcod, stats_appelli.dtappello;
+	GROUP BY stats_appelli.cdscod, stats_appelli.adcod, stats_appelli.dtappello
+	ORDER BY cdscod, adcod, dtappello ASC;
 
 	
-/* VIEW: NUMERO DI APPELLI ED ISCRIZIONI PER CDS */
+/* VIEW: NUMBER OF EXAMS, ENROLLMENTS, AVERAGE OF ENROLLMETS PER CDS */
 DROP VIEW IF EXISTS query1_cds_appelli_iscrizioni;
 CREATE VIEW IF NOT EXISTS query1_cds_appelli_iscrizioni AS
 
@@ -22,7 +24,7 @@ CREATE VIEW IF NOT EXISTS query1_cds_appelli_iscrizioni AS
 	GROUP BY stats_appelli_agg.cdscod, STRFTIME('%Y', stats_appelli_agg.dtappello);
 	
 	
-/* VIEW: NUMERO DI APPELLI ED ISCRIZIONI PER CDS ED AD */
+/* VIEW: NUMBER OF EXAMS, ENROLLMENTS, AVERAGE OF ENROLLMETS PER CDS AND AD */
 DROP VIEW IF EXISTS query1_cds_ad_appelli_iscrizioni;
 CREATE VIEW IF NOT EXISTS query1_cds_ad_appelli_iscrizioni AS
 
@@ -31,15 +33,3 @@ CREATE VIEW IF NOT EXISTS query1_cds_ad_appelli_iscrizioni AS
 		ROUND(CAST(SUM(stats_appelli_agg.numero_iscritti) AS REAL) / CAST(COUNT(*) AS REAL), 3) AS media_iscrizioni_appello
 	FROM stats_appelli_agg
 	GROUP BY stats_appelli_agg.cdscod, stats_appelli_agg.adcod, STRFTIME('%Y', stats_appelli_agg.dtappello);
-
-	
-/* VIEW: NUMERO DI ISCRITTI PER CIASCUN APPELLO */
-DROP VIEW IF EXISTS query1_appelli_iscritti;
-CREATE VIEW IF NOT EXISTS query1_appelli_iscritti AS
-
-	SELECT stats_appelli_agg.cdscod, stats_appelli_agg.adcod, stats_appelli_agg.dtappello, 
-		SUM(stats_appelli_agg.numero_iscritti) AS numero_iscritti
-	FROM stats_appelli_agg
-	GROUP BY stats_appelli_agg.cdscod, stats_appelli_agg.adcod, stats_appelli_agg.dtappello
-	ORDER BY cdscod, adcod, dtappello ASC;
-
